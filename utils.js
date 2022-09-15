90a721ceef83be6b6fd8a90e84f0351d1be87ac7
ethanol <ethan.gard@jane.app>
2022-09-15 18:59:53 -0400

    add function to the utils.js file

---
+++ utils.js
@@ -0,0 +1,21 @@
+// Added functions
+
+function product(numA, numB) {
+  return numA * numB;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function power(num1, num2) {
+  return num1 ** num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
